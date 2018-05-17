5ca0230d8a28b6a2daf2bb6b2f2fe44d76c53763
piter_1316 <piter_1316@wp.pl>
2018-05-17 12:01:18 +0200

    f2

---
+++ src/main/java/com/company/enroller/model/Meeting.java
@@ -0,0 +1,82 @@
+package com.company.enroller.model;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+@Table(name = "meeting")
+public class Meeting {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+
+	@Column
+	private String title;
+
+	@Column
+	private String description;
+
+	@Column
+	private String date;
+
+	@JsonIgnore
+	@ManyToMany(mappedBy = "meetings")
+	Set<Participant> participants = new HashSet<>();
+
+	public long getId() {
+		return id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public void addParticipant(Participant participant) {
+		this.participants.add(participant);
+	}
+
+	public void removeParticipant(Participant participant) {
+		this.participants.remove(participant);
+	}
+
+	public Collection<Participant> getParticipants() {
+		return participants;
+	}
+
+}
---
+++ src/main/java/com/company/enroller/persistence/ParticipantService.java
@@ -0,0 +1,46 @@
+package com.company.enroller.persistence;
+
+import java.util.Collection;
+
+import org.hibernate.Transaction;
+import org.springframework.stereotype.Component;
+
+import com.company.enroller.model.Participant;
+
+@Component("participantService")
+public class ParticipantService {
+
+	DatabaseConnector connector;
+
+	public ParticipantService() {
+		connector = DatabaseConnector.getInstance();
+	}
+
+	public Collection<Participant> getAll() {
+		return connector.getSession().createCriteria(Participant.class).list();
+	}
+
+	public Participant findByLogin(String login) {
+		return (Participant) connector.getSession().get(Participant.class, login);
+	}
+
+	public Participant add(Participant participant) {
+		Transaction transaction = connector.getSession().beginTransaction();
+		connector.getSession().save(participant);
+		transaction.commit();
+		return participant;
+	}
+
+	public void update(Participant participant) {
+		Transaction transaction = connector.getSession().beginTransaction();
+		connector.getSession().merge(participant);
+		transaction.commit();
+	}
+
+	public void delete(Participant participant) {
+		Transaction transaction = connector.getSession().beginTransaction();
+		connector.getSession().delete(participant);
+		transaction.commit();
+	}
+
+}
